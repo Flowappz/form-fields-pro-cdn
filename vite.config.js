f4e101eb6bd8da1705d3bef13c652aafe29c2dd3
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2024-11-26 10:25:03 +0600

    vite config file modified

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,34 @@
-import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
-import { resolve } from 'path';
-import fs from 'fs';
+import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { resolve } from 'path'
+import fs from 'fs'
 
 // Check if there's a main.ts file in the src directory root
-const rootMainFile = fs.existsSync(resolve(__dirname, 'src/cookie-consent.ts'))
-  ? { root: resolve(__dirname, 'src/cookie-consent.ts') }
-  : {};
+const rootMainFile = fs.existsSync(resolve(__dirname, 'src/form-fields-pro-cdn.js')) ? { root: resolve(__dirname, 'src/form-fields-pro-cdn.js') } : {}
 
 // Gather versioned folders in src directory
 const versionFolders = fs
-  .readdirSync(resolve(__dirname, 'src'), { withFileTypes: true })
-  .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => dirent.name);
+    .readdirSync(resolve(__dirname, 'src'), { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name)
 
 export default defineConfig({
-  plugins: [cssInjectedByJsPlugin()],
-  build: {
-    target: 'es2015',
-    rollupOptions: {
-      // Combine root main file (if it exists) with versioned folders as inputs
-      input: {
-        ...rootMainFile,
-        ...Object.fromEntries(
-          versionFolders.map((version) => [
-            version,
-            resolve(__dirname, `src/${version}/cookie-consent.ts`)
-          ])
-        )
-      },
-      output: {
-        entryFileNames: ({ name }) => (name === 'root' ? 'cookie-consent.js' : `${name}/cookie-consent.js`),
-        manualChunks: undefined,
-        dir: resolve(__dirname, 'dist')
-      }
+    plugins: [cssInjectedByJsPlugin()],
+    build: {
+        target: 'es2015',
+        rollupOptions: {
+            // Combine root main file (if it exists) with versioned folders as inputs
+            input: {
+                ...rootMainFile,
+                ...Object.fromEntries(versionFolders.map((version) => [version, resolve(__dirname, `src/${version}/form-fields-pro-cdn.js`)])),
+            },
+            output: {
+                entryFileNames: ({ name }) => (name === 'root' ? 'form-fields-pro-cdn.js' : `${name}/form-fields-pro-cdn.js`),
+                manualChunks: undefined,
+                dir: resolve(__dirname, 'dist'),
+            },
+        },
+        sourcemap: false,
+        minify: 'terser',
     },
-    sourcemap: false,
-    minify: 'terser'
-  }
-});
+})
