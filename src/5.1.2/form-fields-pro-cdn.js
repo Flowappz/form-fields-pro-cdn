ab07fb87cff798aeaf2c14c050bd2764c3a50d78
nadimshuvo <nadimshuvo1997@gmail.com>
2026-08-10 20:22:26 +0600

    added rule to use at least 2 letters in Name field

--- src/5.1.2/form-fields-pro-cdn.js
+++ src/5.1.2/form-fields-pro-cdn.js
@@ -55,8 +55,8 @@ async function buildSubmissionHeaders(siteId, formId) {
 const EMAIL_PATTERN_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const URL_PATTERN_REGEX =
     /^(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/
-/** Plain text / Name: at least 3 Unicode letters (rejects "123", allows "Eve"). */
-const PLAIN_TEXT_MIN_LETTERS = 3
+/** Plain text / Name: at least 2 Unicode letters (rejects "123", allows "Eve/Ed/Jo"). */
+const PLAIN_TEXT_MIN_LETTERS = 2
 
 /** @type {Record<string, Promise<void>>} */
 const __ffpAssetCache = {}
